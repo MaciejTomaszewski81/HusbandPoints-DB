e04065efc25ed1aed06133ea227dba019ec5bf66
soho.tomaszewski@gmail.com <Debiledwa2>
2022-06-29 18:51:32 +0200

    First commit

---
+++ src/test/java/pl/tomaszewski/demo/HusbandPointsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomaszewski.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HusbandPointsDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
